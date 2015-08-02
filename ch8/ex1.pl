3291307b967fc4ca596a9d1576aa26075228ad57
William Shoemaker <william.shoemaker@gmail.com>
2015-08-02 01:49:27 -0500

    Complete Chapter 8, Excercise 1

--- ch8/ex1.pl
+++ ch8/ex1.pl
@@ -6,3 +6,34 @@
 #   If the user chooses to send the output to a scalar, at the end of the program
 #   print the scalar’s value to standard output.
 
+use IO::Tee;
+use DateTime;
+use Getopt::Long;
+
+my %options;
+my $string = '';
+my $dt = DateTime->now();
+my @outputs;
+
+GetOptions (    \%options,
+                'log=s',
+                'scalar',
+           );
+
+if (defined($options{log})) {
+    open my $log_fh, '>>', $options{log}
+        or die "Could not open $options{log}";
+    push @outputs, $log_fh;
+}
+if ($options{scalar}) {
+    open my $scalar_fh, '>>', \$string;
+    push @outputs, $scalar_fh;
+}
+
+$tee_fh = IO::Tee->new( @outputs );
+print $tee_fh "Today is " . $dt->day_name . ", " .  $dt->ymd('/') . "\n";
+
+if ( $string ne '' ) {
+    print "$string";
+}
+
