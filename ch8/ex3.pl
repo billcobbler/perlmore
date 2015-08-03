83866d714ea70d1a9ccf49014f8e3d8e42ac76f6
William Shoemaker <william.shoemaker@gmail.com>
2015-08-02 22:17:01 -0500

    Completed Chapter 8

--- ch8/ex3.pl
+++ ch8/ex3.pl
@@ -1,6 +1,27 @@
 #!/usr/bin/perl
 #
-#   Write a program that takes in multiple directorynames from the command line, then
-#   prints out their contents. Use a function that takes a directoryhandle reference that
+#   Write a program that takes in multiple directory names from the command line, then
+#   prints out their contents. Use a function that takes a directory handle reference that
 #   you made using IO::Dir
 
+use strict;
+use warnings;
+use IO::Dir;
+use Getopt::Long;
+
+my @dirs;
+
+GetOptions ( 'directory=s' => \@dirs );
+foreach my $dir ( split(/,/, join(',',@dirs)) ) {
+    print "Contents of $dir\n";
+    read_dir( new IO::Dir($dir) );
+    print "\n";
+}
+
+
+sub read_dir {
+    my $dir_fh = shift;
+    while( defined( my $file = $dir_fh->read ) ) {
+        print "$file\n";
+    }
+}
