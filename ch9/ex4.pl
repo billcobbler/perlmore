92d86114839c7f523bf5eef1cc9f2a519643821c
William Shoemaker <william.shoemaker@gmail.com>
2015-08-03 23:40:29 -0500

    Completed Chapter 9

--- ch9/ex4.pl
+++ ch9/ex4.pl
@@ -1,17 +1,68 @@
 #!/usr/bin/perl
-#   Modifythe recursive directorydumping routine so it shows the nested directories
+#   Modifythe recursive directory dumping routine so it shows the nested directories
 #   through indentation. An empty directory should show up as:
 #
 #   sandbar, an empty directory
 #
-#   while a nonemptydirectoryshould appear with nested contents, indented two
+#   while a nonempty directory should appear with nested contents, indented two
 #   spaces:
 #
 #   uss_minnow, with contents:
-#    anchor
-#    broken_radio
-#    galley, with contents:
-#       captain_crunch_cereal
-#       gallon_of_milk
-#       tuna_fish_sandwich
-#    life_preservers
+#     anchor
+#     broken_radio
+#     galley, with contents:
+#        captain_crunch_cereal
+#        gallon_of_milk
+#        tuna_fish_sandwich
+#     life_preservers
+
+my $dir = $ARGV[0];
+
+dump_data_for_path($dir, data_for_path($dir));
+
+sub data_for_path {
+    my $path = shift;
+    
+    if (-f $path or -l $path) {     #file or symbolic link
+        return undef;
+    }
+
+    if (-d $path) {
+        my %directory;
+        opendir PATH, $path or die "Cannot opendir $path: $!";
+        my @names = readdir PATH;
+        closedir PATH;
+        for my $name (@names) {
+            next if $name eq '.' or $name eq '..';
+            $directory{$name} = data_for_path("$path/$name");
+        }
+        return \%directory;
+    }
+    warn "$path is neither a file nor a directory\n";
+    return undef;
+}
+
+sub dump_data_for_path {        
+    my $path = shift;
+    my $data = shift;
+    my $prefix = shift || "";
+    
+    print "$prefix$path";
+    
+    if (not defined $data) { # plain file
+        print "\n";
+        return;
+    }
+    
+    my %directory = %$data;
+    
+    if (%directory) {
+        print ", with contents of:\n";
+        for (sort keys %directory) {
+            dump_data_for_path("$path/$_", $directory{$_}, "$prefix  ");
+        } 
+    } else {
+        print ", an empty directory\n";
+    }
+}
+
