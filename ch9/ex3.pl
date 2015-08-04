5fa77f3e4f1de74dd3e8be9d4412007578386836
William Shoemaker <william.shoemaker@gmail.com>
2015-08-03 22:53:29 -0500

    Completed Chapter 9, Exercise 3

--- ch9/ex3.pl
+++ ch9/ex3.pl
@@ -11,3 +11,20 @@
 #
 #   Be sure you don’t mangle the data! If the input includes the Professor and the
 #   skipper, the output should have them listed in that order, with that capitalization.
+
+use strict;
+use warnings;
+
+my @sorted =
+    map $_->[0],
+    sort { $a->[1] cmp $b->[1] }
+    map { 
+            my $string = $_;
+            $string =~ tr/A-Z/a-z/; # force all lowercase
+            $string =~ tr/a-z//cd; # strip all but a-z from the string
+            [$_,$string];
+        } 
+    @ARGV;
+
+print join("\n",@sorted);
+print "\n";
